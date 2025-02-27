9523de5a4e79462fabd726517d58b557c0b89fb3
ebadifardin <mr.ebadifardin@gmail.com>
2025-02-27 07:35:43 -0500

    AAD(HOMES/VIEWS):add function show category

--- homes/views.py
+++ homes/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Catgory
 # Create your views here.
+def show_catgory(request):
+    cate_obj=Catgory.objects.all()
+    contaxe={
+        "cate":cate_obj
+    }
+    return render(request,'base.html',contaxe)

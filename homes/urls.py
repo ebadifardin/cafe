54bd82e4cf9b1e1750f5463b3de6e0f01ff95d98
ebadifardin <mr.ebadifardin@gmail.com>
2025-02-28 06:10:45 -0500

    FIX(HOMES/URLS):config urls for views

--- homes/urls.py
+++ homes/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import show_catgory
+from .views import show_catgory,post
 
 urlpatterns = [
     path('', show_catgory, name='home'),
-    
+    path('',post,name='createpoll')
 ]
